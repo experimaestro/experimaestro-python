58a6fd29c2c85d1697cf9624ac1294977038b302
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-02-20 18:36:13 +0100

    First .circleci version

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import configparser
-import logging
 import os
 import os.path as op
 import platform
@@ -11,11 +10,35 @@ from shutil import copyfile
 
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
+from setuptools.command.install import install
 
-logging.basicConfig(level=logging.DEBUG)
+# --- Read information
 
+config = configparser.ConfigParser()
+config.read('cpp/config.ini')
+informations = config["informations"]
+author = config["author"]
+version = informations["version"]
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+# --- Custom build classes
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        global version
+
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != version:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, version
+            )
+            sys.exit(info)
 
 
 class CMakeExtension(Extension):
@@ -80,32 +103,34 @@ class CMakeBuild(build_ext):
         # Building
         subprocess.check_call(['cmake', '--build', '.', '--target', 'experimaestro_shared'] + build_args,
                               cwd=self.build_temp)
+
         print()  # Add an empty line for cleaner output
 
-config = configparser.ConfigParser()
-config.read('cpp/config.ini')
-informations = config["informations"]
-author = config["author"]
+# --- Setup
 
-def experimaestro_test_suite():
-    import unittest
-    print(sys.path)
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
-
-setup(name='experimaestro',
-    version=informations["version"],
+setup(
+    # Basic informatoin
+    name='experimaestro',
+    version=version,
     description=long_description,
     long_description_content_type="text/markdown",
     author=author["name"],
     author_email=author["email"],
     url=informations["url"],
+
+    # Packages
     packages=['experimaestro'],
     package_dir = {'experimaestro': 'experimaestro'},
-    # add custom build_ext command
-    # test_suite="setup.experimaestro_test_suite",
-    cmdclass=dict(build_ext=CMakeBuild),
+
+    # Allows to use a custom build step
     ext_modules=[CMakeExtension('experimaestro', 'cpp')],
+
+    # We do not allow archives
     zip_safe=False,
+
+    # Version verification
+    cmdclass={
+        'verify': VerifyVersionCommand,
+        'build_ext': CMakeBuild
+    }
 )
