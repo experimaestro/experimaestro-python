bb5d66e3243520a6ee3fe4db8cbfb71f6da6160b
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-12-11 23:17:12 +0100

    Cleanup

--- docker/build-ssh.sh
+++
@@ -1,8 +0,0 @@
-cd /root
-tar xJf libssh-${LIBSSH_VERSION}.tar.xz 
-cd  libssh-${LIBSSH_VERSION}
-mkdir build
-cd build
-cmake -DWITH_STATIC_LIB=OFF -DCMAKE_BUILD_TYPE=Release ..
-make -j${COMPILE_THREADS}
-make install
