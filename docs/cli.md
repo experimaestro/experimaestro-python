4c05ff49979384ce2ee1aae57c47cb264cd63adc
Benjamin Piwowarski <benjamin@piwowarski.fr>
2021-01-22 13:34:40 +0100

    Command to remove orphan jobs

---
+++ docs/cli.md
@@ -0,0 +1,15 @@
+# Command Line Interface
+
+## Dealing with orphans
+
+Check for tasks that are not part of any experimental plan in the given
+experimental folder.
+
+```
+Usage: experimaestro orphans [OPTIONS] PATH
+
+Options:
+  --size      Show size of each folder
+  --clean     Prune the orphan folders
+  --show-all  Show even not orphans
+```
--- experimaestro/__main__.py
+++ experimaestro/__main__.py
@@ -1,13 +1,12 @@
+from itertools import chain
+from shutil import rmtree
 import click
 import logging
 from functools import update_wrapper
 from pathlib import Path
-import importlib
-import importlib.machinery
+import subprocess
 import json
 
-from .utils import logger
-from experimaestro.core.types import ObjectType
 from experimaestro.core.objects import Config, ConfigInformation
 import experimaestro
 
@@ -84,6 +83,52 @@ def rpyc_server(unix_path, clean):
     from experimaestro.rpyc import start_server
 
     start_server(unix_path, clean=clean)
+
+
+@click.option("--show-all", is_flag=True, help="Show even not orphans")
+@click.option("--clean", is_flag=True, help="Prune the orphan folders")
+@click.option("--size", is_flag=True, help="Show size of each folder")
+@click.argument("path", type=Path)
+@cli.command()
+def orphans(path: Path, clean: bool, size: bool, show_all: bool):
+    """Check for tasks that are not part of an experimental plan"""
+
+    jobspath = path / "jobs"
+
+    def getjobs(path: Path):
+        return ((str(p.relative_to(path)), p) for p in path.glob("*/*") if p.is_dir())
+
+    def show(key: str, prefix=""):
+        if size:
+            print(
+                prefix,
+                subprocess.check_output(["du", "-hs", key], cwd=jobspath)
+                .decode("utf-8")
+                .strip(),
+                sep=None,
+            )
+        else:
+            print(prefix, key, sep=None)
+
+    xpjobs = set(
+        key
+        for key, value in chain(
+            *[getjobs(p) for p in (path / "xp").glob("*/jobs") if p.is_dir()]
+        )
+    )
+
+    found = 0
+    for key, job in getjobs(jobspath):
+        if key not in xpjobs:
+            show(jobspath, key)
+            if clean:
+                rmtree(job)
+        else:
+            if show_all:
+                show(key, "[not orphan] ")
+            found += 1
+
+    print(f"{found} jobs are not orphans")
 
 
 def main():
