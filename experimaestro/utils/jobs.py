98056c83ad3c64e6aa8a6632a28589b5ec8e8c16
Benjamin Piwowarski <benjamin@piwowarski.fr>
2022-05-12 21:10:11 +0200

    Fix job utils

--- experimaestro/utils/jobs.py
+++ experimaestro/utils/jobs.py
@@ -7,7 +7,7 @@ from threading import Condition
 
 def jobmonitor(output: TaskOutput):
     """Follow the progress of a job"""
-    from tqdm import tqdm
+    from tqdm.autonotebook import tqdm
 
     cv = Condition()
     job = output.__xpm__.job
@@ -15,6 +15,7 @@ def jobmonitor(output: TaskOutput):
     class LocalListener(Listener):
         def job_state(self, job):
             with cv:
+                # Just notify when something happens
                 cv.notify()
 
     listener = LocalListener()
@@ -26,19 +27,24 @@ def jobmonitor(output: TaskOutput):
 
         job.scheduler.addlistener(listener)
 
-        with tqdm(total=10000) as reporter:
-            while job.state.notstarted():
-                with cv:
-                    cv.wait(timeout=5000)
+        while job.state.notstarted():
+            with cv:
+                cv.wait(timeout=5000)
 
-            while not job.state.finished():
-                delta = int(job.progress[0].progress * 100) - lastprogress
-                if delta >= 0:
-                    reporter.update(delta)
+        if not job.state.finished():
+            with tqdm(total=10000) as reporter:
+                while not job.state.finished():
+                    if job.progress and job.progress[0]:
+                        delta = int(job.progress[0].progress * 100) - lastprogress
+                    else:
+                        delta = 0
 
-                # Wait for an event
-                with cv:
-                    cv.wait(timeout=5000)
+                    if delta >= 0:
+                        reporter.update(delta)
+
+                    # Wait for an event
+                    with cv:
+                        cv.wait(timeout=5000)
 
         if job.state.value != JobState.DONE.value:
             raise RuntimeError(
