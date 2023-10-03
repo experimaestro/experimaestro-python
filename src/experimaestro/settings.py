e0928dd37c6a00ee30be8e04690693f883ee5990
Benjamin Piwowarski <benjamin@piwowarski.fr>
2023-10-03 15:46:54 +0200

    feat: workspace settings

--- src/experimaestro/settings.py
+++ src/experimaestro/settings.py
@@ -3,7 +3,7 @@ from omegaconf import OmegaConf
 from dataclasses import dataclass, field
 from functools import lru_cache
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List
 
 
 @dataclass
@@ -22,8 +22,18 @@ class ServerSettings:
 
 
 @dataclass
+class WorkspaceSettings:
+    id: str
+    """The workspace identifier"""
+
+    path: Path
+    """The workspace path"""
+
+
+@dataclass
 class Settings:
     server: ServerSettings = field(default_factory=ServerSettings)
+    workspaces: List[WorkspaceSettings] = field(default_factory=list)
 
 
 @lru_cache()
@@ -39,3 +49,16 @@ def get_settings(path: Optional[Path] = None) -> Settings:
 
         conf = OmegaConf.load(path)
         return OmegaConf.merge(schema, conf)
+
+
+def get_workspace(id: Optional[str]) -> WorkspaceSettings:
+    """Return the workspace settings given an id (or None for the default one)"""
+    workspaces = get_settings().workspaces
+    if workspaces:
+        if id is None:
+            return workspaces[0]
+        for workspace in workspaces:
+            if id == workspace.id:
+                return workspace
+
+    return None
