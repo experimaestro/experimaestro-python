b63b1534a0d4f44c988148c5dcf0971e4fee6bc0
Benjamin Piwowarski <benjamin@piwowarski.fr>
2025-08-29 08:01:59 +0200

    test: adding a test that should fail

---
+++ src/experimaestro/tests/test_generators.py
@@ -0,0 +1,33 @@
+from experimaestro import Config, Task, Param, Meta, Path, field, PathGenerator
+from experimaestro.scheduler.workspace import Workspace
+from experimaestro.settings import Settings, WorkspaceSettings
+import pytest
+from experimaestro.scheduler import RunMode
+
+
+class Validation(Config):
+    best_checkpoint: Meta[Path] = field(default_factory=PathGenerator("index"))
+
+
+class Learner(Task):
+    validation: Param[Validation]
+    x: Param[int]
+
+
+def test_generators_reuse():
+    # We have one way to select the best model
+    validation = Validation()
+
+    workspace = Workspace(
+        Settings(),
+        WorkspaceSettings("test_generators_reuse", path=Path("/tmp")),
+        run_mode=RunMode.DRY_RUN,
+    )
+
+    with pytest.raises((ValueError, KeyError)):
+        # OK, the path is generated depending on Learner with x=1
+        Learner(x=1, validation=validation).submit(workspace=workspace)
+
+        # Here we have a problem...
+        # the path is still the previous one
+        Learner(x=2, validation=validation).submit(workspace=workspace)
